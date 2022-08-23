99342e7793b89841f477accc95f472328c0c2a9d
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-23 16:48:52 -0700

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,26 +1,40 @@
 //
-// Created by Marvish Chandra on 8/11/22.
+// Created by Marvish Chandra on 8/23/22.
 //
 
 import Foundation
 
 func init{
-
+    footageOwner.init()
+    footageTitle.init()
+    footageQuality.init()
+    footageLength.init()
 }
 
-
-class footageCharacteristics {
-
-    func footageDetails {
-
-        
-}
-    struct otherInformation {
+struct otherInformation {
         myObj1 = fileType
         myObj2 = scene
         myObj3 = fileSize
         myObj4 = fileLocation
     }
+
+
+
+
+class footageCharacteristics {
+
+    func first_footage_scene {}
+    func second_footage_scene {}
+    func third_footage_scene {}
+    func fourth_footage_scene {}
+    func five_footage_scene {}
+    func six_footage_scene {}
+    func seven_footage_scene {}
+    func eight_footage_scene {}
+    func nine_footage_scene {}
+    func tenth_footage_scene {}
+
+}
 
 
 

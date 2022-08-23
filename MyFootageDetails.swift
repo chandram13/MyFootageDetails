69f771d56de8ce08478849dd195339f25dc18e75
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-23 16:40:03 -0700

    Add files via upload

---
+++ MyFootageDetails.swift
@@ -0,0 +1,18 @@
+//
+//  MySonicShakeApp.swift
+//  MySonicShake
+//
+//  Created by Marvish Chandra on 8/11/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct MyFootageDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ main.swift
@@ -0,0 +1,27 @@
+//
+// Created by Marvish Chandra on 8/11/22.
+//
+
+import Foundation
+
+func init{
+
+}
+
+
+class footageCharacteristics {
+
+    func footageDetails {
+
+        
+}
+    struct otherInformation {
+        myObj1 = fileType
+        myObj2 = scene
+        myObj3 = fileSize
+        myObj4 = fileLocation
+    }
+
+
+
+
